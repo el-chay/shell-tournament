8d44eb7b30e146fd0e28c1a6e1cf0bb517bb037c
Antonio Chay <achay@pindrop.com>
2017-11-10 11:24:20 -0500

    magic, have no clue but it works

--- self-edit.bash
+++ self-edit.bash
@@ -5,4 +5,7 @@ echo Hello there ...
 if grep -q @@DELETE-START@@ $0; then
     (sleep 10; sed /@@DELETE-START@@/,/@@DELETE-END@@/d) &
 fi
+echo You can see this only once
 # @@DELETE-END@@
+
+echo 'Bye bye!'
